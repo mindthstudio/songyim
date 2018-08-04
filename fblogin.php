6af21e219d50a4ecf128846a5fa8160ad928d4ff
Mindth <mindthztudio@gmail.com>
2018-08-05 06:43:30 +0700

    show table

--- fblogin.php
+++ fblogin.php
@@ -5,7 +5,57 @@
 
 // $parse = parse_url($myurl, PHP_URL_FRAGMENT);
 
+$indicesServer = array('PHP_SELF', 
+'argv', 
+'argc', 
+'GATEWAY_INTERFACE', 
+'SERVER_ADDR', 
+'SERVER_NAME', 
+'SERVER_SOFTWARE', 
+'SERVER_PROTOCOL', 
+'REQUEST_METHOD', 
+'REQUEST_TIME', 
+'REQUEST_TIME_FLOAT', 
+'QUERY_STRING', 
+'DOCUMENT_ROOT', 
+'HTTP_ACCEPT', 
+'HTTP_ACCEPT_CHARSET', 
+'HTTP_ACCEPT_ENCODING', 
+'HTTP_ACCEPT_LANGUAGE', 
+'HTTP_CONNECTION', 
+'HTTP_HOST', 
+'HTTP_REFERER', 
+'HTTP_USER_AGENT', 
+'HTTPS', 
+'REMOTE_ADDR', 
+'REMOTE_HOST', 
+'REMOTE_PORT', 
+'REMOTE_USER', 
+'REDIRECT_REMOTE_USER', 
+'SCRIPT_FILENAME', 
+'SERVER_ADMIN', 
+'SERVER_PORT', 
+'SERVER_SIGNATURE', 
+'PATH_TRANSLATED', 
+'SCRIPT_NAME', 
+'REQUEST_URI', 
+'PHP_AUTH_DIGEST', 
+'PHP_AUTH_USER', 
+'PHP_AUTH_PW', 
+'AUTH_TYPE', 
+'PATH_INFO', 
+'ORIG_PATH_INFO') ; 
 
+echo '<table cellpadding="10">' ; 
+foreach ($indicesServer as $arg) { 
+    if (isset($_SERVER[$arg])) { 
+        echo '<tr><td>'.$arg.'</td><td>' . $_SERVER[$arg] . '</td></tr>' ; 
+    } 
+    else { 
+        echo '<tr><td>'.$arg.'</td><td>-</td></tr>' ; 
+    } 
+} 
+echo '</table>' ; 
 // $url=getCurrentURL();
 // var_dump(parse_url($url));
 // var_dump(parse_url($url, PHP_URL_SCHEME));
@@ -43,8 +93,8 @@
 //     return 'https://' . $host . $script . $params;
 // }
 
-$dir = dirname(__FILE__);
-echo "<p>Full path to this dir: " . $dir . "</p>";
-echo "<p>Full path to a .htpasswd file in this dir: " . $dir . "/.htpasswd" . "</p>";
+// $dir = dirname(__FILE__);
+// echo "<p>Full path to this dir: " . $dir . "</p>";
+// echo "<p>Full path to a .htpasswd file in this dir: " . $dir . "/.htpasswd" . "</p>";
 
 ?>
