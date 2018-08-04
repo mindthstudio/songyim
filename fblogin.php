7baab7ad717baff890c96e4383f87437631bbdce
Mindth <mindthztudio@gmail.com>
2018-08-05 05:48:06 +0700

    request fragment

--- fblogin.php
+++ fblogin.php
@@ -1,15 +1,24 @@
 <?php 
 
-print_r($_SERVER["REQUEST_URI"]);
+
+$myurl = $_SERVER['REQUEST_URI'];
+
+$parse = parse_url($myurl, PHP_URL_FRAGMENT);
+
+echo "<pre>";
+print_r($prase);
+echo "</pre>";
+
 $access_token = $_GET['access_token'];
+
 //redirect("http://songyim.eventhive.in.th/index.php/Signin/fbsignin?");
 
-$url  = "https://graph.facebook.com/v3.1/me?fields=id%2Cname&access_token=$access_token";
-$content = file_get_contents($url);
+// $url  = "https://graph.facebook.com/v3.1/me?fields=id%2Cname&access_token=$access_token";
+// $content = file_get_contents($url);
 
-echo "$url";
-echo "<br>";
+// echo "$url";
+// echo "<br>";
 
-echo $content;
+// echo $content;
 
 ?>
