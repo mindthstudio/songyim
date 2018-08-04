eaf02ba57c3523818564a9a5acb2f6aade09c868
Mindth <mindthztudio@gmail.com>
2018-08-05 05:20:12 +0700

    update app

--- fblogin.php
+++ fblogin.php
@@ -1,6 +1,14 @@
 <?php 
 
-echo $_GET['code'];
+$access_token $_GET['code'];
 //redirect("http://songyim.eventhive.in.th/index.php/Signin/fbsignin?");
+
+$url  = "https://graph.facebook.com/v3.1/me?fields=id%2Cname&access_token=$access_token";
+$curl = curl_init($url);
+$content = curl_exec($curl);
+
+echo "<pre>";
+print_r($count);
+echo "</pre>";
 
 ?>
