89aad8dfabfc4fc9c9b28c094715312f31dbb630
Mindth <mindthztudio@gmail.com>
2018-08-05 05:22:52 +0700

    fixed error

--- fblogin.php
+++ fblogin.php
@@ -1,11 +1,10 @@
 <?php 
 
-$access_token $_GET['code'];
+$access_token = $_GET['code'];
 //redirect("http://songyim.eventhive.in.th/index.php/Signin/fbsignin?");
 
 $url  = "https://graph.facebook.com/v3.1/me?fields=id%2Cname&access_token=$access_token";
-$curl = curl_init($url);
-$content = curl_exec($curl);
+$content = file_get_contents($url);
 
 echo "<pre>";
 print_r($content);
