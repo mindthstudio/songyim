9e4fbaf48509f9db2c1bdd01783f9ac8908f1fea
Mindth <mindthztudio@gmail.com>
2018-08-05 04:43:37 +0700

    update app

--- fblogin.php
+++ fblogin.php
@@ -1,8 +1,6 @@
 <?php 
 
-echo "Hello World";
+echo $_GET['code'];
 //redirect("http://songyim.eventhive.in.th/index.php/Signin/fbsignin?");
 
 ?>
-
-<a href="https://www.facebook.com/v3.1/dialog/oauth?client_id=376379539535403&redirect_uri=https://songyim.herokuapp.com/fblogin.php" class="btn btn-default btn-block btn-facebook">เข้าสู่ระบบด้วย Facebook</a>
